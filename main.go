589c961746592120afe408ecb28d67712d5c85a8
song <55rsearchco@gmail.com>
2023-10-31 14:53:13 +0900

    google OAuth 이용해 로그인 구현

--- main.go
+++ main.go
@@ -1,5 +1,100 @@
 package main
 
-func main() {
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/gorilla/pat"
+	"github.com/joho/godotenv"
+	"github.com/urfave/negroni"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var googleOauthConfig oauth2.Config
+
+func googleLoginHandler(w http.ResponseWriter, r *http.Request) {
+	state := generateStateOauthCookie(w)
+	url := googleOauthConfig.AuthCodeURL(state)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	expiration := time.Now().Add(1 * 24 * time.Hour)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := &http.Cookie{
+		Name:    "oauthstate",
+		Value:   state,
+		Expires: expiration,
+	}
+	http.SetCookie(w, cookie)
+	return state
+}
+
+func googleAuthCallback(w http.ResponseWriter, r *http.Request) {
+	oauthstate, _ := r.Cookie("oauthstate")
+
+	if state := r.FormValue("state"); state != oauthstate.Value {
+		log.Printf("invalid google oauth state cookie:%s state:%s\n", oauthstate.Value, state)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	data, err := getGoogleUserInfo(r.FormValue("code"))
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprint(w, string(data))
+}
+
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
+
+func getGoogleUserInfo(code string) ([]byte, error) {
+	token, err := googleOauthConfig.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to Exchange %s\n", err.Error())
+	}
+
+	resp, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to Get UserInfo %s\n", err.Error())
+	}
+
+	return io.ReadAll(resp.Body)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	googleOauthConfig = oauth2.Config{
+		RedirectURL:  "http://localhost:3000/auth/google/callback",
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+
+	mux := pat.New()
+	mux.HandleFunc("/auth/google/login", googleLoginHandler)
+	mux.HandleFunc("/auth/google/callback", googleAuthCallback)
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+
+	http.ListenAndServe(":3000", n)
 }
